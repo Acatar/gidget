5c694d242d0842c849cd27bb48a22d2dc4ec7d52
&y <andes.collab@gmail.com>
2015-08-07 18:34:18 -0400

    gives more control to developers when passing data to navigate

--- release/gidget.js
+++ release/gidget.js
@@ -1,4 +1,4 @@
-/*! gidget-builder 2015-08-06 */
+/*! gidget-builder 2015-08-07 */
 Hilary.scope("gidget").register({
     name: "IGidget",
     dependencies: [ "Blueprint" ],
@@ -838,56 +838,60 @@ Hilary.scope("gidget").register({
         dependencies: [ "BaseRouteEngine", "is", "uriHelper" ],
         factory: function(RouteEngine, is, uriHelper) {
             var start, onLoad, addEventListeners, clickHandler, popstateHandler, routeEngine;
-            start = function() {
-                addEventListeners();
-                onLoad();
-            };
-            onLoad = function() {
-                routeEngine.navigate(location.href);
-            };
-            clickHandler = function(event) {
-                if (is.string(event.target.localName) && event.target.localName === "a") {
-                    event.preventDefault();
-                    routeEngine.navigate(event.target.href);
-                }
-            };
-            popstateHandler = function(event) {
-                if (is.string(event.state)) {
-                    event.preventDefault();
-                    routeEngine.navigate(event.state, null, false);
-                } else if (is.object(event.state) && is.defined(event.state.path)) {
-                    event.preventDefault();
-                    routeEngine.navigate(event.state.path);
-                }
-            };
-            addEventListeners = function() {
-                document.addEventListener("click", clickHandler, false);
-                window.addEventListener("popstate", popstateHandler, false);
-            };
-            routeEngine = new RouteEngine({
-                start: start
-            });
-            routeEngine.navigate = function(path, data, pushStateToHistory) {
-                var state = data || {}, uri = uriHelper.parseUri(path);
-                if (is.not.defined(pushStateToHistory)) {
-                    pushStateToHistory = true;
-                }
-                if (!uri.host || uri.host === document.location.host) {
-                    state.uri = uri;
-                    state.title = window.title;
-                } else {
-                    window.location = path;
-                    return;
-                }
-                if (pushStateToHistory) {
-                    history.pushState(state.uri, state.title, state.uri.relativePath);
-                }
-                routeEngine.resolveAndExecuteRoute(state.uri, "get");
-            };
-            routeEngine.dispose = function() {
-                document.removeEventListener("click", clickHandler, false);
-                window.removeEventListener("popstate", popstateHandler, false);
-            };
+            (function() {
+                start = function() {
+                    addEventListeners();
+                    onLoad();
+                };
+                onLoad = function() {
+                    routeEngine.navigate(location.href);
+                };
+                clickHandler = function(event) {
+                    if (is.string(event.target.localName) && event.target.localName === "a") {
+                        event.preventDefault();
+                        routeEngine.navigate(event.target.href);
+                    }
+                };
+                popstateHandler = function(event) {
+                    if (is.string(event.state)) {
+                        event.preventDefault();
+                        routeEngine.navigate(event.state, null, false);
+                    } else if (is.object(event.state) && is.defined(event.state.path)) {
+                        event.preventDefault();
+                        routeEngine.navigate(event.state.path);
+                    }
+                };
+                addEventListeners = function() {
+                    document.addEventListener("click", clickHandler, false);
+                    window.addEventListener("popstate", popstateHandler, false);
+                };
+            })();
+            (function() {
+                routeEngine = new RouteEngine({
+                    start: start
+                });
+                routeEngine.navigate = function(path, data, pushStateToHistory) {
+                    var state = data || {}, uri = uriHelper.parseUri(path);
+                    if (!uri.host || uri.host === document.location.host) {
+                        state.uri = state.uri || uri;
+                        state.title = state.title || window.title;
+                    } else {
+                        window.location = path;
+                        return;
+                    }
+                    if (is.not.defined(pushStateToHistory)) {
+                        pushStateToHistory = true;
+                    }
+                    if (pushStateToHistory) {
+                        history.pushState(state.uri, state.title, state.uri.relativePath);
+                    }
+                    routeEngine.resolveAndExecuteRoute(state.uri, "get");
+                };
+                routeEngine.dispose = function() {
+                    document.removeEventListener("click", clickHandler, false);
+                    window.removeEventListener("popstate", popstateHandler, false);
+                };
+            })();
             return routeEngine;
         }
     });
--- src/routeEngines/DefaultRouteEngine.js
+++ src/routeEngines/DefaultRouteEngine.js
@@ -13,67 +13,82 @@
                 popstateHandler,
                 routeEngine;
 
-            start = function () {
-                addEventListeners();
-                onLoad();
-            };
+            // PRIVATE members
+            (function () {
+                start = function () {
+                    addEventListeners();
+                    onLoad();
+                };
 
-            onLoad = function () {
-                routeEngine.navigate(location.href);
-            };
+                onLoad = function () {
+                    routeEngine.navigate(location.href);
+                };
 
-            clickHandler = function (event) {
-                if (is.string(event.target.localName) && event.target.localName === 'a') {
-                    event.preventDefault();
-                    routeEngine.navigate(event.target.href);
-                }
-            };
+                clickHandler = function (event) {
+                    if (is.string(event.target.localName) && event.target.localName === 'a') {
+                        event.preventDefault();
+                        routeEngine.navigate(event.target.href);
+                    }
+                };
 
-            popstateHandler = function (event) {
-                if (is.string(event.state)) {
-                    event.preventDefault();
-                    routeEngine.navigate(event.state, null, false);
-                } else if (is.object(event.state) && is.defined(event.state.path)) {
-                    event.preventDefault();
-                    routeEngine.navigate(event.state.path);
-                }
-            };
+                popstateHandler = function (event) {
+                    if (is.string(event.state)) {
+                        event.preventDefault();
+                        routeEngine.navigate(event.state, null, false);
+                    } else if (is.object(event.state) && is.defined(event.state.path)) {
+                        event.preventDefault();
+                        routeEngine.navigate(event.state.path);
+                    }
+                };
 
-            addEventListeners = function () {
-                document.addEventListener('click', clickHandler, false);
-                window.addEventListener('popstate', popstateHandler, false);
-            };
+                addEventListeners = function () {
+                    document.addEventListener('click', clickHandler, false);
+                    window.addEventListener('popstate', popstateHandler, false);
+                };
+            }());
 
-            routeEngine = new RouteEngine({
-                start: start
-            });
-            routeEngine.navigate = function (path, data, pushStateToHistory) {
-                var state = data || {},
-                    uri = uriHelper.parseUri(path);
+            // PUBLIC members
+            (function () {
+                // create an instance of BaseRouteEngine and set the
+                // start, navigate and dispose behaviors
+                routeEngine = new RouteEngine({
+                    start: start
+                });
 
-                if (is.not.defined(pushStateToHistory)) {
-                    pushStateToHistory = true;
-                }
+                routeEngine.navigate = function (path, data, pushStateToHistory) {
+                    var state = data || {},
+                        uri = uriHelper.parseUri(path);
 
-                if (!uri.host || (uri.host === document.location.host)) {
-                    state.uri = uri;
-                    state.title = window.title;
-                } else {
-                    window.location = path;
-                    return;
-                }
+                    if (!uri.host || (uri.host === document.location.host)) {
+                        // if the uri is relative, or if the host matches the current host
+                        // then we are navigating within the SPA
+                        state.uri = state.uri || uri;
+                        state.title = state.title || window.title;
+                    } else {
+                        // otherwise, we must be leaving the SPA
+                        window.location = path;
+                        return;
+                    }
 
-                if (pushStateToHistory) {
-                    history.pushState(state.uri, state.title, state.uri.relativePath);
-                }
+                    if (is.not.defined(pushStateToHistory)) {
+                        // default behavior for history is true
+                        pushStateToHistory = true;
+                    }
 
-                routeEngine.resolveAndExecuteRoute(state.uri, 'get');
-            };
+                    if (pushStateToHistory) {
+                        // add the state to the browser history (i.e. to support back and forward)
+                        history.pushState(state.uri, state.title, state.uri.relativePath);
+                    }
 
-            routeEngine.dispose = function () {
-                document.removeEventListener('click', clickHandler, false);
-                window.removeEventListener('popstate', popstateHandler, false);
-            };
+                    // execute the route
+                    routeEngine.resolveAndExecuteRoute(state.uri, 'get');
+                };
+
+                routeEngine.dispose = function () {
+                    document.removeEventListener('click', clickHandler, false);
+                    window.removeEventListener('popstate', popstateHandler, false);
+                };
+            }());
 
             return routeEngine;
         }
